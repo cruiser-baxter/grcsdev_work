830852225c66dad50fbc73567d867d17a3f53f1f
Cruiser Baxter <cruiser.j.baxter@gmail.com>
2023-01-04 15:54:26 -0800

    reverted to previous version and modified to from x,y to x,y,z coordinates.

--- Test Project/src/making_changes/Point.java
+++ Test Project/src/making_changes/Point.java
@@ -3,25 +3,32 @@ package making_changes;
 public class Point {
     private double x;
     private double y;
+    private double z;
 
-    public Point(double x, double y) {
+    public Point(double x, double y, double z) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
     public double getX() {
         return x;
     }
 
-    public void setX(double x) {
-        this.x = x;
-    }
-
     public double getY() {
         return y;
     }
 
-    public void setY(double y) {
-        this.y = y;
+    public double getZ() {
+        return z;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
     }
 }
