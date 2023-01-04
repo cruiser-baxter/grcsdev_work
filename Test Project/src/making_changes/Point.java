db3211daa4e5ad8a4cd661b75fe6cbcde974ba57
Cruiser Baxter <cruiser.j.baxter@gmail.com>
2023-01-04 15:57:16 -0800

    The last commit I did a merge as instructed by the video, but it actually merged with old code instead of re-basing or overwriting it.

--- Test Project/src/making_changes/Point.java
+++ Test Project/src/making_changes/Point.java
@@ -34,28 +34,4 @@ public class Point {
                 '}';
     }
 
-    @Override
-    public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Point point = (Point) o;
-        return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
 }
